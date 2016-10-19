cdd705e9732bf16c163d546d048b9f8da7b7f5a3
Nicolasdejean <nicolas.dejean@epitech.eu>
2016-10-19 10:55:45 +0200

    first pull request: changed the example ProtocolBuffer

--- examples/ProtoBuffer/protobuffer.ino
+++ examples/ProtoBuffer/protobuffer.ino
@@ -1,10 +1,13 @@
 #include <TheThingsNetwork.h>
 
-TheThingsNetwork ttn(Serial1, Serial, TTN_FP_EU868);
-TheThingsMessage msg(Serial);
-
 const byte appEui[8] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x00, 0x11, 0x86 };
 const byte appKey[16] = { 0xEF, 0x4C, 0xAA, 0x85, 0xE1, 0xB2, 0x26, 0x53, 0x98, 0xE2, 0x3C, 0xD1, 0x31, 0x50, 0xCA, 0xDF };
+
+#define loraSerial Serial1
+#define debugSerial Serial
+
+TheThingsNetwork ttn(loraSerial, debugSerial, TTN_FP_EU868);
+TheThingsMessage msg(debugSerial);
 
 api_Measurement measurement = api_Measurement_init_default;
 
@@ -12,16 +15,23 @@ void setup()
 {
   pinMode(2, INPUT);
 
-  Serial1.begin(57600);
-  Serial.begin(9600);
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
 
-  while (!Serial);
+  // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000);
 
+  //Set callback for incoming message
+  msg.onMessage(message);
+
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
+
+  debugSerial.println("-- JOIN");
   ttn.join(appEui, appKey);
 
-  msg.ChoseMessage(&measurement, all);
-
-  msg.onMessage(message);
+  //Chose message you want to send
+  msg.ChoseMessage(&measurement, all /* Water, Humidity, Temperature, Motion*/);
 }
 
 void loop()
@@ -37,6 +47,7 @@ void loop()
   measurement.humidity = humidity;
 
   msg.showValues(measurement);
+
   msg.encodeMessage(measurement);
 
   msg.sendMessage();
